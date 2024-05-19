5a673e3f1a33ae15c086166c6c89751b647f1f0f
Richard Lopez <rl3020@columbia.edu>
2024-05-19 15:35:42 -0700

    Upload repo to github for gpt tutorial.

---
+++ app.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import json
+
+
+load_dotenv()  # load env variables
+client = OpenAI()
+
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    response_format={"type": "json_object"},
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant that likes to rhyme in every response."},
+        {"role": "system", "content": "Your response must be in JSON where there is a 'code' field that has any code and a 'rhyme_explanation' field where the code is explained but in a way that rhymes."},
+        {"role": "user", "content": "print hello world in python"},
+        {"role": "user", "content": "now do it in java"},
+    ]
+)
+
+completion = json.loads(completion.choices[0].message.content)
+print("Code: ", completion["code"])
+print("Expl: ", completion["rhyme_explanation"])
